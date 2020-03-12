06b2d24c7969a4b30dd1283cbd53da2a1567056b
IanWernecke <IanWernecke@protonmail.com>
2020-03-11 21:26:22 -0400

    Adding a publish script for a shortcut

---
+++ publish.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+py=python3
+
+# ensure the directory exists
+if [[ ! -d dist ]];
+then
+    echo "Directory not found: dist";
+    exit 1;
+fi
+
+# ensure there is something to publish
+if [[ $(ls dist/*.whl | wc -l) -eq 0 ]];
+then
+    echo "No wheels found in directory: dist";
+    exit 1;
+fi
+
+# ensure twine is installed
+if [[ $($py -m pip list | grep -E '^twine' | wc -l ) -eq 0 ]];
+then
+    echo "Required module not found: twine"
+    exit 1
+fi
+
+$py -m twine upload dist/*.whl
